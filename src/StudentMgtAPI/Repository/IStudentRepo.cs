57f7f77df2093189f4b5ee3e855e9328c7707b10
Gabriel Isaac <iamkemical1@gmail.com>
2020-07-01 20:31:21 +0100

    Added Repository and Services

---
+++ src/StudentMgtAPI/Repository/IStudentRepo.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using StudentMgtAPI.Models;
+
+namespace StudentMgtAPI.Repository
+{
+    public interface IStudentRepo
+    {
+        IEnumerable<StudentModel> GetAllStudent();
+        StudentModel GetStudentById(int id);
+        void CreateStudent(StudentModel studentModel);
+        void DeleteStudent(StudentModel studentModel);
+        void UpdateStudent(StudentModel studentModel);
+    }
+}
---
+++ src/StudentMgtAPI/Services/StudentService.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using StudentMgtAPI.Models;
+using StudentMgtAPI.Repository;
+
+namespace StudentMgtAPI.Services
+{
+    public class StudentService : IStudentRepo
+    {
+        public void CreateStudent(StudentModel studentModel)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteStudent(StudentModel studentModel)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<StudentModel> GetAllStudent()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public StudentModel GetStudentById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateStudent(StudentModel studentModel)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
